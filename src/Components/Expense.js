a2f0934517ecf1a8e28e4f0c3e662bddc93b7efe
Priyobrato Das <priyodas12@gmail.com>
2021-02-25 19:56:13 +0530

    added basic component

--- src/Components/Expense.js
+++ src/Components/Expense.js
@@ -3,9 +3,21 @@ import React from 'react'
 function Expense() {
   return (
     <div>
-      <h3>Your Balance</h3>
-      <div className="balance">
-        <h4>$1000</h4>
+      <div>
+        <h3>Your Balance</h3>
+        <div className="balance">
+          <h4>$1000</h4>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>Income</h3>
+          <div>$3000</div>
+        </div>
+        <div>
+          <h3>Expense</h3>
+          <div>$2000</div>
+        </div>
       </div>
     </div>
   )
